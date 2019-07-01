12b3d178c36843258d783b6da8c605207d79f823
Daniel Rebelo <danielrebelo@outlook.com>
2019-07-01 22:28:12 +0200

    Add BitBay.net icon

--- Tofu/AccountCell.swift
+++ Tofu/AccountCell.swift
@@ -26,6 +26,7 @@ private func imageForAccount(_ account: Account) -> UIImage {
     case .some("AWS"): return UIImage(named: "AWS")!
     case .some("Backblaze"): return UIImage(named: "Backblaze")!
     case .some("Binance.com"): return UIImage(named: "Binance")!
+    case .some("BitBayAuth"): return UIImage(named: "BitBay")!
     case .some("Bitbucket"): return UIImage(named: "Bitbucket")!
     case .some("Bitwarden"): return UIImage(named: "Bitwarden")!
     case .some("Coinbase"): return UIImage(named: "Coinbase")!
