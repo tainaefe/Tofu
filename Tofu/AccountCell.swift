55525b0176cd1e0cf457cd21bb18a901e0e83189
Calle Luks <calle@luks.in>
2021-01-28 09:46:03 +0100

    Look up image names using a dictionary

--- Tofu/AccountCell.swift
+++ Tofu/AccountCell.swift
@@ -1,86 +1,93 @@
 import UIKit
 
+let imageNames = [
+    "Adobe ID": "Adobe",
+    "Allegro": "Allegro",
+    "Amazon": "Amazon",
+    "AnonAddy": "AnonAddy",
+    "Atlassian": "Atlassian",
+    "AWS": "AWS",
+    "Amazon Web Services": "AWS",
+    "Backblaze": "Backblaze",
+    "Basecamp's+Launchpad": "Basecamp",
+    "Binance.com": "Binance",
+    "BitBayAuth": "BitBay",
+    "Bitbucket": "Bitbucket",
+    "Bittrex": "Bittrex",
+    "Bitwarden": "Bitwarden",
+    "Cloudflare": "Cloudflare",
+    "Coinbase": "Coinbase",
+    "CorporateTrust": "CorporateTrust",
+    "CyDIS": "CyDIS",
+    "DigitalOcean": "DigitalOcean",
+    "DNSimple": "DNSimple",
+    "Dropbox": "Dropbox",
+    "Discord": "Discord",
+    "hub.docker.com": "Docker",
+    "Electronic Arts": "ElectronicArts",
+    "Epic+Games": "EpicGames",
+    "Evernote": "Evernote",
+    "Facebook": "Facebook",
+    "Fastmail": "FastMail",
+    "Firefox": "Firefox",
+    "gandi.net": "Gandi",
+    "Gitea": "Gitea",
+    "GitHub": "GitHub",
+    "gitlab.com": "GitLab",
+    "Google": "Google",
+    "GreenAddress": "GreenAddress",
+    "Hack The Box": "HackTheBox",
+    "Heroku": "Heroku",
+    "HEY": "HEY",
+    "Hostek": "Hostek",
+    "Hover": "Hover",
+    "HumbleBundle": "HumbleBundle",
+    "IFTTT": "IFTTT",
+    "Instagram": "Instagram",
+    "Intercom": "Intercom",
+    "JetBrains+Account": "JetBrains",
+    "Kickstarter": "Kickstarter",
+    "LastPass": "LastPass",
+    "LinkedIn": "LinkedIn",
+    "LinodeManager": "Linode",
+    "LocalBitcoins": "LocalBitcoins",
+    "Mastodon": "Mastodon",
+    "Microsoft": "Microsoft",
+    "Name.com": "Name.com",
+    "Nextcloud": "Nextcloud",
+    "NiceHash": "NiceHash",
+    "NiceHash - New platform": "NiceHash",
+    "NordPass": "NordPass",
+    "ownCloud": "ownCloud",
+    "Paladin Extensions": "PaladinExtensions",
+    "Parler": "Parler",
+    "PayPal": "PayPal",
+    "Privacy.com": "Privacy",
+    "ProtonMail": "ProtonMail",
+    "Reddit": "Reddit",
+    "Robinhood": "Robinhood",
+    "Slack": "Slack",
+    "Snapchat": "Snapchat",
+    "STACK": "STACK",
+    "Stripe": "Stripe",
+    "Surfshark": "Surfshark",
+    "Time4VPS": "Time4VPS",
+    "TorGuard": "TorGuard",
+    "Tresorit": "Tresorit",
+    "Tumblr": "Tumblr",
+    "TurboTax": "TurboTax",
+    "Tutanota": "Tutanota",
+    "Twitter": "Twitter",
+    "Ubisoft": "Ubisoft",
+    "WordPress": "WordPress",
+]
+
 private func imageForAccount(_ account: Account) -> UIImage? {
-    switch account.issuer {
-    case .some("Adobe ID"): return UIImage(named: "Adobe")!
-    case .some("Allegro"): return UIImage(named: "Allegro")!
-    case .some("Amazon"): return UIImage(named: "Amazon")!
-    case .some("AnonAddy"): return UIImage(named: "AnonAddy")!
-    case .some("Atlassian"): return UIImage(named: "Atlassian")!
-    case .some("AWS"), .some("Amazon Web Services"): return UIImage(named: "AWS")!
-    case .some("Backblaze"): return UIImage(named: "Backblaze")!	
-    case .some("Basecamp's+Launchpad"): return UIImage(named: "Basecamp")!
-    case .some("Binance.com"): return UIImage(named: "Binance")!
-    case .some("BitBayAuth"): return UIImage(named: "BitBay")!
-    case .some("Bitbucket"): return UIImage(named: "Bitbucket")!
-    case .some("Bittrex"): return UIImage(named: "Bittrex")!
-    case .some("Bitwarden"): return UIImage(named: "Bitwarden")!
-    case .some("Cloudflare"): return UIImage(named: "Cloudflare")!
-    case .some("Coinbase"): return UIImage(named: "Coinbase")!
-    case .some("CorporateTrust"): return UIImage(named: "CorporateTrust")!
-    case .some("CyDIS"): return UIImage(named: "CyDIS")!
-    case .some("DigitalOcean"): return UIImage(named: "DigitalOcean")!
-    case .some("DNSimple"): return UIImage(named: "DNSimple")!
-    case .some("Dropbox"): return UIImage(named: "Dropbox")!
-    case .some("Discord"): return UIImage(named: "Discord")!
-    case .some("hub.docker.com"): return UIImage(named: "Docker")!
-    case .some("Electronic Arts"): return UIImage(named: "ElectronicArts")!
-    case .some("Epic+Games"): return UIImage(named: "EpicGames")!
-    case .some("Evernote"): return UIImage(named: "Evernote")!
-    case .some("Facebook"): return UIImage(named: "Facebook")!
-    case .some("Fastmail"): return UIImage(named: "FastMail")!
-    case .some("Firefox"): return UIImage(named: "Firefox")!
-    case .some("gandi.net"): return UIImage(named: "Gandi")!
-    case .some("Gitea"): return UIImage(named: "Gitea")!
-    case .some("GitHub"): return UIImage(named: "GitHub")!
-    case .some("gitlab.com"): return UIImage(named: "GitLab")!
-    case .some("Google"): return UIImage(named: "Google")!
-    case .some("GreenAddress"): return UIImage(named: "GreenAddress")!
-    case .some("Hack The Box"): return UIImage(named: "HackTheBox")!
-    case .some("Heroku"): return UIImage(named: "Heroku")!
-    case .some("HEY"): return UIImage(named: "HEY")!
-    case .some("Hostek"): return UIImage(named: "Hostek")!
-    case .some("Hover"): return UIImage(named: "Hover")!
-    case .some("HumbleBundle"): return UIImage(named: "HumbleBundle")!
-    case .some("IFTTT"): return UIImage(named: "IFTTT")!
-    case .some("Instagram"): return UIImage(named: "Instagram")!
-    case .some("Intercom"): return UIImage(named: "Intercom")!
-    case .some("JetBrains+Account"): return UIImage(named: "JetBrains")!
-    case .some("Kickstarter"): return UIImage(named: "Kickstarter")!
-    case .some("LastPass"): return UIImage(named: "LastPass")!
-    case .some("LinkedIn"): return UIImage(named: "LinkedIn")!
-    case .some("LinodeManager"): return UIImage(named: "Linode")!
-    case .some("LocalBitcoins"): return UIImage(named: "LocalBitcoins")!
-    case .some("Mastodon"): return UIImage(named: "Mastodon")!
-    case .some("Microsoft"): return UIImage(named: "Microsoft")!
-    case .some("Name.com"): return UIImage(named: "Name.com")!
-    case .some("Nextcloud"): return UIImage(named: "Nextcloud")!
-    case .some("NiceHash"), .some("NiceHash - New platform"): return UIImage(named: "NiceHash")!
-    case .some("NordPass"): return UIImage(named: "NordPass")!
-    case .some("ownCloud"): return UIImage(named: "ownCloud")!
-    case .some("Paladin Extensions"): return UIImage(named: "PaladinExtensions")!
-    case .some("Parler"): return UIImage(named: "Parler")!
-    case .some("PayPal"): return UIImage(named: "PayPal")!
-    case .some("Privacy.com"): return UIImage(named: "Privacy")!
-    case .some("ProtonMail"): return UIImage(named: "ProtonMail")!
-    case .some("Reddit"): return UIImage(named: "Reddit")!
-    case .some("Robinhood"): return UIImage(named: "Robinhood")!
-    case .some("Slack"): return UIImage(named: "Slack")!
-    case .some("Snapchat"): return UIImage(named: "Snapchat")!
-    case .some("STACK"): return UIImage(named: "STACK")!
-    case .some("Stripe"): return UIImage(named: "Stripe")!
-    case .some("Surfshark"): return UIImage(named: "Surfshark")!
-    case .some("Time4VPS"): return UIImage(named: "Time4VPS")!
-    case .some("TorGuard"): return UIImage(named: "TorGuard")!
-    case .some("Tresorit"): return UIImage(named: "Tresorit")!
-    case .some("Tumblr"): return UIImage(named: "Tumblr")!
-    case .some("TurboTax"): return UIImage(named: "TurboTax")!
-    case .some("Tutanota"): return UIImage(named: "Tutanota")!
-    case .some("Twitter"): return UIImage(named: "Twitter")!
-    case .some("Ubisoft"): return UIImage(named: "Ubisoft")!
-    case .some("WordPress"): return UIImage(named: "WordPress")!
-    default: return nil
+    if let issuer = account.issuer,
+       let imageName = imageNames[issuer] {
+        return UIImage(named: imageName)!
     }
+    return nil
 }
 
 private func imageWithColor(_ color: UIColor, size: CGSize) -> UIImage {
